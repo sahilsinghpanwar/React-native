484ba1a001356d3fa6c5a6216703c84bf29b442e
sahil panwar <sahilpanwar0211@gmail.com>
2026-08-06 15:32:44 +0530

    update all fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,38 +3,29 @@ import { Link } from "expo-router";
 import { styled } from "nativewind";
 import { Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
-
 const SafeAreaView = styled(RNSafeAreaView);
+
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-5xl font-sans-extrabold ">Home</Text>
       <Link
         href={"/onboading"}
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
       >
         Go to Onboading
       </Link>
       <Link
         href={"/(auth)/sign-in"}
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
       >
         Go to Sign in
       </Link>
       <Link
         href={"/(auth)/sign-up"}
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
       >
         Go to Sing up
-      </Link>
-
-      <Link href="/subscriptions/spotify">Spotify Subscription</Link>
-      <Link
-        href={{ pathname: "/subscriptions/[id]", params: { id: "claude" } }}
-      >
-        Claude Max Subscription
       </Link>
     </SafeAreaView>
   );
