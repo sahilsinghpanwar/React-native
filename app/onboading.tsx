ed1f133ed94ca55d2fad689cf0c3830701055008
sahil panwar <sahilpanwar0211@gmail.com>
2026-08-06 09:43:33 +0530

    tab navigation is working

---
+++ app/onboading.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Onboading = () => {
+  return (
+    <View>
+      <Text>Onboading</Text>
+    </View>
+  );
+};
+
+export default Onboading;
---
+++ metro.config.js
@@ -0,0 +1,8 @@
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativewind } = require("nativewind/metro");
+ 
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+ 
+// module.exports = withNativewind(config);
+module.exports = withNativewind(config, { input: "./global.css" });
